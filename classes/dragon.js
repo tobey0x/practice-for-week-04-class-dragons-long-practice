b198cabf895ee4adff5c24a8e7cd0944155c51ee
tobey0x <omoniyiadejengbe@gmail.com>
2022-11-05 01:05:01 +0100

    initial dragon commit

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,4 +1,38 @@
 // Your code here
+class Dragon {
+    constructor(name, color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    breathesFire() {
+        return `${this.name} breathes fire everywhere! BURN!!!!`;
+    }
+
+    static getDragons(...dragons) {
+        let dragonNames = [];
+        dragons.forEach((dragon) => {
+            dragonNames.push(dragon.name);
+        });
+        return dragonNames
+    }
+}
+
+
+
+const puff = new Dragon("Puff", "green");
+const toothless = new Dragon("Toothless", "black");
+console.log(Dragon.getDragons(puff, toothless));
+
+/****************************************************************************/
+/******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
+
+try {
+    module.exports = Dragon;
+} catch {
+    module.exports = null;
+}
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
