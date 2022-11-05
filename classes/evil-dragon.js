233487549d9c628f82921cf9d1b92eb2ac39113d
tobey0x <omoniyiadejengbe@gmail.com>
2022-11-05 01:06:36 +0100

    evil dragon commit

--- classes/evil-dragon.js
+++ classes/evil-dragon.js
@@ -1,1 +1,39 @@
+const Dragon = require("./dragon");
+
 // Your code here
+class EvilDragon extends Dragon {
+    constructor(name, color, evilDoings, nemesis) {
+        super(name, color);
+        this.evilDoings = evilDoings;
+        this.nemesis = nemesis;
+    }
+    dontInviteThemOverForDinner() {
+        this.evilDoings.forEach((evilDoing) => {
+            console.log(`${this.name} will ${evilDoing}`);
+        });
+    }
+    burnsNemesis() {
+        return `${this.name} destroys ${this.nemesis} with fire! WHOOOSH!!!`;
+    }
+}
+
+const enderDragon = new EvilDragon(
+    "Ender Dragon",
+    "black",
+    [
+        "spit a fireball at Steve",
+        "rule over all the Endermen",
+        "destroy all blocks in its way",
+    ],
+    "Steve"
+);
+console.log(enderDragon);
+enderDragon.dontInviteThemOverForDinner();
+console.log(enderDragon.breathesFire());
+console.log(enderDragon.burnsNemesis());
+
+try {
+    module.exports = EvilDragon;
+} catch {
+    module.exports = null;
+}
